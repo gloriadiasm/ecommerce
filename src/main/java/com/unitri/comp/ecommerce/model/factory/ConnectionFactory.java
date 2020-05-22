c61296eadf694534496db3d7070d6072e9f4789a
gloriadiasm <gloriamaridias@gmail.com>
2020-05-22 13:17:12 -0300

    implementing all

--- src/main/java/com/unitri/comp/ecommerce/model/factory/ConnectionFactory.java
+++ src/main/java/com/unitri/comp/ecommerce/model/factory/ConnectionFactory.java
@@ -6,12 +6,13 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    public Connection getConnection() {
+    public Connection getConnection() throws SQLException{
         try {
             return DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/ecommerce", "root", "root1234");
+                    "jdbc:mysql://localhost:3306/ecommerce?useTimezone=true&serverTimezone=UTC", "root", "root123");
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
+
 }
